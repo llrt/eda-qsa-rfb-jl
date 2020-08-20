ebfcc1b5e92a6f64e74bc2720a7b62d966a102f9
Leandro Loriato <leandroloriato@gmail.com>
2020-08-19 22:59:08 -0300

    Importacao de dados. Analise inicial, de exemplo

--- README.md
+++ README.md
@@ -4,19 +4,18 @@ Esse é um repositório em Julia com análise exploratória (EDA) em cima da bas
 
 A análise é vagamente inspirada em outras análises disponíveis em R, como [esta](https://www.curso-r.com/blog/2019-09-20-qsacnpj/). 
 
-Para baixar a base completa do QSA, você pode utilizar o excelente [Brasil.io](https://brasil.io/), um portal de dados públicos brasileiros. A base do QSA em CSV neste site está [aqui](https://data.brasil.io/dataset/socios-brasil/_meta/list.html). 
+Para baixar a base completa do QSA, você pode utilizar o excelente [Brasil.io](https://brasil.io/), um portal de dados públicos ("libertos") brasileiros. A base do QSA em CSV neste site está [aqui](https://data.brasil.io/dataset/socios-brasil/_meta/list.html). 
 
 **Atenção:** a base QSA em CSV é **muito** grande (cerca de 2.4 GB para a parte de empresas, 0,4 GB para a parte de sócios, 0,7 GB para a parte de CNAE) - tenha certeza que possui espaço disponível em seu HD e memória RAM suficiente para processá-la. Caso não tenha, é possível gerar uma base *trimmada* (enxugada, recortada com algum critério específico) usando pacotes como o [`qsacnpj`](https://github.com/georgevbsantiago/qsacnpj) no R. Neste repositório o autor gentilmente disponibiliza também os arquivos estáticos base em formato SQLite - que na minha experiência é muito melhor para fazer consultas e agregações com menor necessidade de memória RAM, motivo pelo qual será esta a base que utilizaremos.
 
 
 ## Base utilizada
 
-Como dito acima, a base que será utilizada nessa EDA é a base QSA em formato SQLite obtida a partir do repositório `qsacnpj`. A base utilizada foi a de [28/04/2020](https://github.com/georgevbsantiago/qsacnpj#base-de-dados-do-cnpj-tratada), com tamanho aproximado de 5,3 GB e 43,9 milhões de CNPJs. Para não deixar pesado o repositório, o arquivo estático *não* será incluído.  
+Como dito acima, a base que será utilizada nessa EDA é a base QSA em formato SQLite obtida a partir do repositório `qsacnpj`. A base utilizada foi a de [04/07/2020](https://github.com/georgevbsantiago/qsacnpj#base-de-dados-do-cnpj-tratada), com tamanho aproximado de 5,3 GB comprimidos (19,1 GB descomprimidos) e 43,9 milhões de CNPJs. Para não deixar pesado o repositório, o arquivo estático *não* será incluído. O esquema relacional se encontra [aqui](https://raw.githubusercontent.com/georgevbsantiago/qsacnpj/master/img/esquema_cnpj.png)   
 
 ## Bibliotecas utilizadas
 
 - **DataFrames**: para criar e manipular estruturas de data frames
-- **CategoricalArrays**: para criar colunas com variáveis categóricas (factors) em data frames
 - **CSV**: para importar arquivos CSV
 - **StatsBase**: para realizar análises estatísticas básicas
 - **Queryverse**: para manipulação de data frames usando um estilo `dplyr/tidyverse`
---
+++ analise.jl
@@ -0,0 +1,95 @@
+# essa é uma análise exploratória (EDA) da base pública de CNPJs (QSA) da Receita Federal do Brasil (RFB)
+
+## carrega bibliotecas relevantes
+using DataFrames # para manipulação de data frames
+#using CSV # para ler CSVs
+using SQLite, Tables # para ler SQLites
+using StatsBase # para estatística básica
+using Queryverse # metapacote para data science
+
+
+## lendo arquivo de entrada
+db = SQLite.DB("bd_dados_qsa_cnpj.db") # carrega o arquivo SQLite com as tabelas
+
+## executa a consulta base e retorna um result set, que será depois usado 
+# para trazer as empresas de interesse
+# IMPORTANTE: como a base é muito grande, para reduzir o uso massivo de memória RAM
+# é crucial ter uma consulta base que já filtre as empresas de principal interesse - 
+# no nosso caso só estamos interessados nas empresas ativas (situacao_cadastral='02')
+# e matrizes (identificador_matriz_filial = '1') - e restrinja os campos de interesse; 
+# como teremos de fazer já a consulta, vamos aproveitar para fazer joins e transformações
+# interessantes
+dados_pj_rs = 
+    DBInterface.execute(db, 
+"
+    SELECT 
+        pj.cnpj, 
+        pj.nome_fantasia, 
+        pj.situacao_cadastral as situacao, 
+        case pj.porte_empresa 
+            when '00' then '?'
+            when '01' then 'MICRO'
+            when '03' then 'PEQUENA'
+            when '05' then 'DEMAIS'
+        end as porte, 
+        case pj.opcao_pelo_simples
+            when '' then 'N'
+            when '0' then 'N'
+            when '5' then 'S'
+            when '7' then 'S'
+            when '6' then 'S - Excluido'
+            when '8' then 'S - Excluido'
+        end as simples,
+        case pj.opcao_pelo_mei 
+            when 'S' then 'S'
+            else 'N'
+        end as MEI,
+        nat_jur.nm_natureza_juridica as nat_juridica,
+        pj.data_inicio_atividade as dat_ini_atividade, 
+        pj.cnae_fiscal as cod_cnae_principal, 
+        cnae.nm_cnae as cnae_principal,
+        cnae.nm_divisao as divisao_cnae_principal,
+        cnae.nm_classe as classe_cnae_principal,
+        cnae.nm_grupo as grupo_cnae_principal,
+        pj.bairro, pj.codigo_municipio as cod_municipio, pj.municipio, pj.uf
+    FROM `cnpj_dados_cadastrais_pj` pj 
+        INNER JOIN `tab_cnae` cnae on cnae.cod_cnae = pj.cnae_fiscal 
+        INNER JOIN `tab_natureza_juridica` nat_jur on nat_jur.cod_subclass_natureza_juridica = pj.codigo_natureza_juridica
+    WHERE 
+        situacao_cadastral='02' -- somente ativas
+        and identificador_matriz_filial='1' -- somente matrizes
+    ORDER BY data_inicio_atividade, cnpj
+    ;
+") # consulta base  
+
+# OBS: o result set somente suporta ser consumido uma única vez (single pass, forware only). 
+# Para usá-lo de novo, é preciso resetá-lo (rodar a query de novo) com: SQLite.reset!(dados_pj_rs)
+
+
+## tendo o result set inicial, podemos executar as análises de interesse
+
+## Análise 1: quantos cartórios temos no Brasil?
+SQLite.reset!(dados_pj_rs) # para recarregar o result set, se necessário
+dados_cartorios = dados_pj_rs |> 
+    @filter(_.cnae_principal=="Cartórios") |>
+    DataFrame
+
+print("O Brasil possui $(nrow(dados_cartorios)) cartórios!")
+
+
+# mas onde eles estão localizados? são muitos porque temos muitas cidades?
+dados_cartorios_uf = dados_cartorios |> 
+    @groupby(_.uf) |> @map({uf=key(_), n_cartorios=length(_)}) |> @orderby_descending(_.n_cartorios)
+
+dados_cartorios_uf_municipios = dados_cartorios |> 
+    @select(:uf, :municipio) |> @unique() |> 
+    @groupby(_.uf) |> @map({uf=key(_), n_municipios=length(_)}) |> @orderby_descending(_.n_municipios)
+
+comparacao_cartorios_uf = dados_cartorios_uf |> 
+    @join(dados_cartorios_uf_municipios, _.uf, _.uf, {_.uf, _.n_cartorios, __.n_municipios}) |> 
+    @orderby_descending(_.n_cartorios) |> DataFrame
+
+Statistics::cor(
+    convert(Array{Float64,1}, comparacao_cartorios_uf.n_cartorios), 
+    convert(Array{Float64,1}, comparacao_cartorios_uf.n_municipios)
+)
